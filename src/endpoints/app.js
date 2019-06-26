dd9a5027db5499da7fbe0e9e454566c20dd24750
Mike Alicea <mynewemail@example.com>
2019-06-26 08:06:02 -0400

    Initial commit after sam init

---
+++ src/endpoints/app.js
@@ -0,0 +1,16 @@
+exports.lambdaHandler = async (event, context) => {
+	try {
+		// const ret = await axios(url);
+		response = {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: 'hello world',
+				// location: ret.data.trim()
+			}),
+		};
+		return response;
+	} catch (err) {
+		console.log(err);
+		return err;
+	}
+};
